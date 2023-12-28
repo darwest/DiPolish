58b585676df83170f07fd4018a0035fdce937cd9
darwest <dashavasuta25@gmail.com>
2023-12-28 13:14:30 +0200

    n

--- server/netlify.js
+++
@@ -1,9 +0,0 @@
-const server = require('./server/server'); // вам потрібно буде замінити це на шлях до вашого реального серверного коду
-
-exports.handler = async function (event, context) {
-  const response = await server.handleRequest(event);
-  return {
-    statusCode: response.statusCode,
-    body: response.body,
-  };
-};
